ed0b4e1de05bfeb280b1bdeb1e74101dc105a2b2
Tony Grimes <acidtone@tonygrimes.com>
2023-03-23 13:16:07 -0600

    added achievement-1 example

---
+++ src/routes/d01-pokemon/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { ditto } from './ditto';
+
+  // Log the following values to the console
+  // Example: Ditto's name
+  console.log(ditto.name);
+
+  // 1. The `id` of Ditto (132)
+  console.log(ditto.id);
+
+  // 2. Ditto's `base_experience` (101)
+  console.log(ditto.base_experience);
+
+  // 3. The `url` of Ditto's `species`
+  console.log(ditto.species.url);
+
+  // 4. The url of Ditto's `front_default` in `sprites`
+  console.log(ditto.sprites.front_default);
+
+  // 5. The `name` of Ditto's only `move` (transform)
+  console.log(ditto.moves[0].move.name);
+
+  // 6. The `name` of the `version` of Ditto's first `held_items` (ultra-moon)
+  console.log(ditto.held_items[0].version_details[0].version.name);
+
+</script>
+<h1>Achievement 1</h1>
+<p>See console for answers.</p>
---
+++ src/routes/d01-pokemon/ditto.js
@@ -0,0 +1,162 @@
+export const ditto = {
+  abilities: [
+    {
+      ability: {
+        name: "imposter",
+        url: "https://pokeapi.co/api/v2/ability/150/"
+      },
+      is_hidden: true,
+      slot: 3
+    }
+  ],
+  base_experience: 101,
+  forms: [
+    {
+      name: "ditto",
+      url: "https://pokeapi.co/api/v2/pokemon-form/132/"
+    }
+  ],
+  game_indices: [
+    {
+      game_index: 132,
+      version: {
+        name: "white-2",
+        url: "https://pokeapi.co/api/v2/version/22/"
+      }
+    }
+  ],
+  height: 3,
+  held_items: [
+    {
+      item: {
+        name: "metal-powder",
+        url: "https://pokeapi.co/api/v2/item/234/"
+      },
+      version_details: [
+        {
+          rarity: 5,
+          version: {
+            name: "ultra-moon",
+            url: "https://pokeapi.co/api/v2/version/30/"
+          }
+        }
+      ]
+    },
+    {
+      item: {
+        name: "quick-powder",
+        url: "https://pokeapi.co/api/v2/item/251/"
+      },
+      version_details: [
+        {
+          rarity: 50,
+          version: {
+            name: "ultra-moon",
+            url: "https://pokeapi.co/api/v2/version/30/"
+          }
+        }
+      ]
+    }
+  ],
+  id: 132,
+  is_default: true,
+  location_area_encounters: "https://pokeapi.co/api/v2/pokemon/132/encounters",
+  moves: [
+    {
+      move: {
+        name: "transform",
+        url: "https://pokeapi.co/api/v2/move/144/"
+      },
+      version_group_details: [
+        {
+          level_learned_at: 1,
+          move_learn_method: {
+            name: "level-up",
+            url: "https://pokeapi.co/api/v2/move-learn-method/1/"
+          },
+          version_group: {
+            name: "scarlet-violet",
+            url: "https://pokeapi.co/api/v2/version-group/25/"
+          }
+        }
+      ]
+    }
+  ],
+  name: "ditto",
+  order: 214,
+  past_types: [],
+  species: {
+    name: "ditto",
+    url: "https://pokeapi.co/api/v2/pokemon-species/132/"
+  },
+  sprites: {
+    back_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png",
+    back_female: null,
+    back_shiny: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/132.png",
+    back_shiny_female: null,
+    front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
+    front_female: null,
+    front_shiny: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png",
+    front_shiny_female: null
+  },
+  stats: [
+    {
+      base_stat: 48,
+      effort: 1,
+      stat: {
+        name: "hp",
+        url: "https://pokeapi.co/api/v2/stat/1/"
+      }
+    },
+    {
+      base_stat: 48,
+      effort: 0,
+      stat: {
+        name: "attack",
+        url: "https://pokeapi.co/api/v2/stat/2/"
+      }
+    },
+    {
+      base_stat: 48,
+      effort: 0,
+      stat: {
+        name: "defense",
+        url: "https://pokeapi.co/api/v2/stat/3/"
+      }
+    },
+    {
+      base_stat: 48,
+      effort: 0,
+      stat: {
+        name: "special-attack",
+        url: "https://pokeapi.co/api/v2/stat/4/"
+      }
+    },
+    {
+      base_stat: 48,
+      effort: 0,
+      stat: {
+        name: "special-defense",
+        url: "https://pokeapi.co/api/v2/stat/5/"
+      }
+    },
+    {
+      base_stat: 48,
+      effort: 0,
+      stat: {
+        name: "speed",
+        url: "https://pokeapi.co/api/v2/stat/6/"
+      }
+    }
+  ],
+  types: [
+    {
+      slot: 1,
+      type: {
+        name: "normal",
+        url: "https://pokeapi.co/api/v2/type/1/"
+      }
+    }
+  ],
+  weight: 40
+}
