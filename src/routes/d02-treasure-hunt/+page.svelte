ef9577579a7a57768c45f5822b071d11e68fb231
Tony Grimes <acidtone@tonygrimes.com>
2023-03-24 11:10:13 -0600

    added ach-1 spoilers

---
+++ src/routes/d02-treasure-hunt/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { queenArianna } from "./disney";
+
+  // Log the following values to the console
+
+  // Example: Queen Arianna's name
+  console.log(queenArianna.name);
+
+  // 1. The ID of Queen Arianna
+  console.log(queenArianna._id);
+
+  // 2. An image of Queen Arianna
+  console.log(queenArianna.imageUrl);
+
+
+  // 3. Queen Arianna's first film
+  console.log(queenArianna.films[0]);
+
+  // 4. Queen Arianna's last TV show
+  console.log(queenArianna.tvShows[queenArianna.tvShows.length - 1]);
+
+  // 5. Queen Arianna's second video game
+  console.log(queenArianna.videoGames[1]);
+</script>
+
+<h1>Achievement 1: Object Value Treasure Hunt</h1>
+<p>See console log to see the treasure!</p>
---
+++ src/routes/d02-treasure-hunt/disney.js
@@ -0,0 +1,30 @@
+export const queenArianna = {
+  films: [
+    "Tangled",
+    "Tangled: Before Ever After"
+  ],
+  shortFilms: [
+    "Tangled Ever After",
+    "Hare Peace"
+  ],
+  tvShows: [
+    "Once Upon a Time",
+    "Tangled: The Series"
+  ],
+  videoGames: [
+    "Disney Princess Enchanting Storybooks",
+    "Hidden Worlds",
+    "Disney Crossy Road",
+    "Kingdom Hearts III"
+  ],
+  parkAttractions: [
+    "Celebrate the Magic",
+    "Jingle Bell, Jingle BAM!"
+  ],
+  allies: [],
+  enemies: [],
+  _id: 308,
+  name: "Queen Arianna",
+  imageUrl: "https://static.wikia.nocookie.net/disney/images/1/15/Arianna_Tangled.jpg",
+  url: "https://api.disneyapi.dev/characters/308"
+}
