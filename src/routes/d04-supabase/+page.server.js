8e193ae9d885501b425e2d57db37888e49dd8b6b
Tony Grimes <acidtone@tonygrimes.com>
2023-03-28 14:22:08 -0600

    tweaks

--- src/routes/d04-supabase/+page.server.js
+++ src/routes/d04-supabase/+page.server.js
@@ -1,7 +1,7 @@
 import { supabase } from "$lib/supabaseClient";
 
 export async function load() {
-  let { data, error } = await supabase.from('countries').select();
+  let { data } = await supabase.from('countries').select();
 
   return {
     countries: data ?? [],
