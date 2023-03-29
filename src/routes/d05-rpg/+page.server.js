e96179d77a3c9519bb5f0aaa136ec5036c1fea34
Tony Grimes <acidtone@tonygrimes.com>
2023-03-29 10:56:24 -0600

    fixes

--- src/routes/d05-rpg/+page.server.js
+++ src/routes/d05-rpg/+page.server.js
@@ -2,7 +2,7 @@ import { supabase } from "$lib/supabaseClient";
 
 export async function load() {
   let { players } = await supabase.from('rpg_player').select();
-  let { items } = await supabase.from('rpg_items').select();
+  let { items } = await supabase.from('rpg_item').select();
 
   return {
     players: players ?? [],
