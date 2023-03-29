49a20430d2bf3aace02231091fbca3567fbecbd6
Tony Grimes <acidtone@tonygrimes.com>
2023-03-29 08:35:32 -0600

    added if

--- src/routes/d04-supabase/+page.svelte
+++ src/routes/d04-supabase/+page.svelte
@@ -4,8 +4,12 @@
 </script>
 
 <h1>Countries</h1>
+{#if data.countries.length > 0}
 <ul>
   {#each data.countries as country}
     <li>{country.id}: {country.name}</li>
   {/each}
 </ul>
+{:else}
+<p>No countries found</p>
+{/if}
